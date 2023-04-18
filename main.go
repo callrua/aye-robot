27bca762d1995fbbbad5960c5b567d83b5757d8b
Ruairi Callaghan <ruairiscallaghan@gmail.com>
2023-04-18 18:58:22 +0100

    cleanup main

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -47,27 +46,6 @@ func main() {
 	}
 
 	// start the server
-	go func() {
-		logger.Println("Starting server on port: ", listenAddr)
-
-		err := server.ListenAndServe()
-		if err != nil {
-			logger.Printf("%s\n", err)
-			os.Exit(1)
-		}
-	}()
-
-	// trap sigterm or interupt and gracefully shutdown the server
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
-
-	// Block until a signal is received.
-	sig := <-c
-	logger.Println("Got signal:", sig)
-
-	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	server.Shutdown(ctx)
-	defer cancel()
+	logger.Println("Starting server on address", *listenAddr)
+	server.ListenAndServe()
 }
